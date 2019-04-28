40101b7a16e7cc694ec810f33dbae2ca4465dda6
jeffersonchaves <jeffersong.s@hotmail.com>
2019-04-28 11:32:12 -0300

    Exercício 6, resolvido.

--- 6.py
+++ 6.py
@@ -3,17 +3,19 @@ Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a ve
 para a viagem.
 '''
 
-distancia_km = 120
+distancia_km = 10
 velocidade_media = 80
 
 
-tempo_viagem = velocidade_media // velocidade_media
+tempo_viagem = distancia_km // velocidade_media
 
 if distancia_km % velocidade_media:
 
     distancia_km_faltantes = distancia_km % velocidade_media
-    tempo_viagem_min = (distancia_km_faltantes / velocidade_media) * 60
+    tempo_viagem_min = int( (distancia_km_faltantes / velocidade_media) * 60 )
 
-    print(tempo_viagem,' - ', tempo_viagem_min)
+    print('tempo de viagem: {}h e {}min '.format(tempo_viagem,tempo_viagem_min))
 
+else:
+    print('tempo de viagem: {}hora(s)'.format(tempo_viagem))
 
