a44935ba427266f534bb3a01b18ff9667f2f290f
jeffersonchaves <jeffersong.s@hotmail.com>
2019-04-28 10:13:48 -0300

    Add exercícios de 1 a 4. Exercícico 1 concluído.

---
+++ 2.py
@@ -0,0 +1,10 @@
+'''
+Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
+'''
+
+#valor_metros = int(input('digite um valor em metros'))
+valor_metros  = 10
+valor_milimetros = valor_metros * 1000
+
+print('valor em metros: ', valor_metros)
+print('valor em milimetros: ', valor_milimetros)
