e0604b586b9ae9d33623e03e5636b2d610dcab15
jeffersonchaves <jeffersong.s@hotmail.com>
2019-04-28 10:28:21 -0300

    Exercícicos 3,4 concluídos.

---
+++ 3.py
@@ -0,0 +1,18 @@
+'''
+Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o
+total em segundos.
+'''
+
+qtd_dias = 1
+qtd_horas = 0
+qtd_minutos = 0
+qtd_segundos = 1
+
+qtd_dias_segundos = qtd_dias * 24 * 60 * 60
+qtd_horas_segundos = qtd_horas * 60 * 60
+qtd_minutos_segundos = qtd_minutos * 60
+
+total_em_segundos = qtd_dias_segundos + qtd_horas_segundos + qtd_minutos_segundos + qtd_segundos
+
+print('quantidade de segundos: ', total_em_segundos)
+
---
+++ 4.py
@@ -0,0 +1,12 @@
+'''
+Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do
+salário e a porcentagem do aumento. Exiba o valor do aumento e do novo salário.
+'''
+
+salario = 1000
+aumento = 0.1
+
+aumento_real = salario * aumento
+salario_real = salario + aumento_real
+
+print(salario_real)
