ef726bafb2d7952a0595def37fac27f25124cc9b
jeffersonchaves <jeffersong.s@hotmail.com>
2019-05-01 06:34:44 -0300

    exercício 6

---
+++ 6a.py
@@ -0,0 +1,13 @@
+'''
+Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada
+para a viagem.
+'''
+distancia_km = 10
+velocidade_media = 80
+
+tempo_viagem = distancia_km / velocidade_media
+
+print('tempo de viagem: ', tempo_viagem)
+
+
+
