c216c87d7b0d7d82252035ff6c7e61d999617262
Jefferson Chaves <jeffersong.s@hotmail.com>
2019-05-18 15:20:49 -0300

    changing files names

--- 01.py
+++ 01.py
@@ -7,7 +7,7 @@ e. ( True  )  2**2 == math.pow(2, 2)
 f. ( False )  False and ( (7 / 2) > 3.5 )
 g. ( True  )  ( math.ceil( 2.5 ) == 3) and (math.sqrt( 9) == 3)
 '''
-import math 
+import math
 
 print(2 * 4 == 24/3)  # a
 print((15 % 4) < (19 % 6))  # b
